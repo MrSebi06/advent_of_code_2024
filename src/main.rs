a4573ae16deefb3bec1b2aa9b8323fd6b4cf5131
sebastian.mihnea <sebastian.mihnea@napta.io>
2024-12-02 17:09:13 +0100

    First day

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use regex::bytes::Regex;
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("src/data").expect("Cannot read the file");
+    let byte_contents = contents.into_bytes();
+    let left: Vec<&[u8]> = Regex::new(r"\W+").unwrap().split(&byte_contents).enumerate().filter(|(i, _)| i % 2 == 0).map(|(_, x)| x).collect();
+    for x in left {
+        println!("{:?}", x);
+    }
+}
