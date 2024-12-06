d181d4ed4cef072c2692344ffc5bc495f8cefc91
sebastian.mihnea <sebastian.mihnea@napta.io>
2024-12-06 17:11:28 +0100

    Part one

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,23 @@ use std::fs;
 
 fn main() {
     let data = fs::read_to_string("src/data").expect("Cannot read the file");
+    let reports: Vec<Vec<usize>> = data.split('\n').map(|x| x.split(' ').map(|y| y.parse().unwrap()).collect()).collect();
+
+    part_one(&reports);
+    part_two();
 }
 
-fn part_one() {}
+fn part_one(reports: &Vec<Vec<usize>>) {
+    let mut safe_sum = 0;
+    for report in reports {
+        let safe = report.windows(2).all(|x| {
+            x[0].abs_diff(x[1]) <= 3 && x[0].abs_diff(x[1]) >= 1
+        });
+        let is_decreasing = report.windows(2).all(|x| x[0] > x[1]);
+        let is_increasing = report.windows(2).all(|x| x[0] < x[1]);
+        safe_sum += (safe && (is_decreasing || is_increasing)) as usize;
+    }
+    println!("{safe_sum}")
+}
 
 fn part_two() {}
