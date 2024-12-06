f0dab9fa143c11c728d341ba3b41c2454e2e6026
sebastian.mihnea <sebastian.mihnea@napta.io>
2024-12-06 15:56:36 +0100

    Second star

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("src/data").expect("Cannot read the file");
-    let mut left: Vec<usize> = Regex::new(r"\W+").unwrap().split(contents.as_str()).enumerate().filter(|(i, _)| i % 2 == 0).map(|(_, x)| x.parse().unwrap()).collect();
-    let mut right: Vec<usize> = Regex::new(r"\W+").unwrap().split(contents.as_str()).enumerate().filter(|(i, _)| i % 2 == 1).map(|(_, x)| x.parse().unwrap()).collect();
+    let left: Vec<usize> = Regex::new(r"\W+").unwrap().split(contents.as_str()).enumerate().filter(|(i, _)| i % 2 == 0).map(|(_, x)| x.parse().unwrap()).collect();
+    let right: Vec<usize> = Regex::new(r"\W+").unwrap().split(contents.as_str()).enumerate().filter(|(i, _)| i % 2 == 1).map(|(_, x)| x.parse().unwrap()).collect();
 
+    part_one(left.clone(), right.clone());
+    part_two(left, right);
+}
+
+fn part_one(mut left: Vec<usize>, mut right: Vec<usize>) {
     let mut sum = 0;
 
     let get_min_index = |x: &Vec<usize>| x.iter().enumerate().min_by(|(_, a), (_, b)| a.cmp(b)).map(|(i, _)| i).unwrap();
@@ -22,5 +27,14 @@ fn main() {
         sum += distance;
     }
     println!("{sum}")
+}
+
+fn part_two(left: Vec<usize>, right: Vec<usize>) {
+    let mut similarity = 0;
+    for l in left {
+        let factor = right.iter().filter(|&&r| r == l).count();
+        similarity += l * factor;
+    }
+    println!("{similarity}")
 }
 
