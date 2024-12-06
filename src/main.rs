4b401179efc01a85ed030eadc36fd6db50d49714
MrSebi06 <mihneasebastian06@gmail.com>
2024-12-06 12:56:48 +0100

    Split left right column

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use regex::bytes::Regex;
+use regex::Regex;
 use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("src/data").expect("Cannot read the file");
-    let byte_contents = contents.into_bytes();
-    let left: Vec<&[u8]> = Regex::new(r"\W+").unwrap().split(&byte_contents).enumerate().filter(|(i, _)| i % 2 == 0).map(|(_, x)| x).collect();
-    for x in left {
-        println!("{:?}", x);
+    let left: Vec<usize> = Regex::new(r"\W+").unwrap().split(contents.as_str()).enumerate().filter(|(i, _)| i % 2 == 0).map(|(_, x)| x.parse::<usize>());
+    let right: Vec<usize> = Regex::new(r"\W+").unwrap().split(contents.as_str()).enumerate().filter(|(i, _)| i % 2 == 1).map(|(_, x)| x.parse()).collect();
+    let lr =  std::iter::zip(left, right);
+    for (l, r) in lr {
+        println!("{l}   {r}");
     }
 }
